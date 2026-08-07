aab7fd547b5f56a8dc4ad8b2ea42b22703af7886
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-08-07 13:51:41 +0700

    fix: export force-dynamic for post detail and API routes

--- src/app/(frontend)/post/[postId]/page.jsx
+++ src/app/(frontend)/post/[postId]/page.jsx
@@ -1,5 +1,7 @@
 import ViewPostPage from '@/views/ViewPostPage'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page({ params }) {
   const resolvedParams = await params
   return <ViewPostPage postId={resolvedParams?.postId} />
