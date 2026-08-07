85911fa7f92fa7607621020f45f9d3345ffcd41b
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-08-07 13:57:19 +0700

    refactor: flatten route group (frontend) into root app routes

--- src/app/(frontend)/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function FrontendLayout({ children }) {
-  return (
-    <div className="mx-auto w-[1126px] max-w-full text-center border-x border-[var(--border)] min-h-[100svh] flex flex-col">
-      {children}
-    </div>
-  )
-}
